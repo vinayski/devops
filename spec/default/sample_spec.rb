a14c4665255508dc86152679640a0146061275e3
SKi <vinayski@gmail.com>
2018-01-26 18:28:43 -0500

    add test cases for graphite

---
+++ spec/default/sample_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe package('httpd'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('apache2'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('httpd'), :if => os[:family] == 'redhat' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('apache2'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe service('graphite-web'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+end
+
+describe service('graphite-carbon'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
