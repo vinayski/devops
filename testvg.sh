d6c0f02c320e4b88d815837c04e8f983e87a2895
SKi <vinayski@gmail.com>
2018-01-26 14:03:08 -0500

    add commands to check status and destroy vm

--- testvg.sh
+++ testvg.sh
@@ -1,2 +1,12 @@
+#add new box
 vagrant box add ubuntu/trusty64
+#list boxes
+vagrant box list
+#start vm
+vagrant up
+#check status
+vagrant status
+# connect to vm
 vagrant ssh
+# destroy
+vagrant destroy -f
