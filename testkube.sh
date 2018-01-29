a337e176dd83fef1f75d160f202d4c9fd1dddf05
SKi <vinayski@gmail.com>
2018-01-29 17:55:28 -0500

    deploy kubeless on your Kubernetes cluster and how to use kubeless to deploy Python and Node.js functions

--- testkube.sh
+++ testkube.sh
@@ -15,3 +15,51 @@ kubectl expose deployment first-deployment --port=80 --type=NodePort
 #Find the port and check the url
 export PORT=$(kubectl get svc first-deployment -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
 curl host01:$PORT
+
+
+#Deploying and using kubeless, the Kubernetes Native serverless framework.
+#Create a kubeless namespace where you will install the controller
+kubectl create ns kubeless
+#install latest
+curl -sL https://github.com/kubeless/kubeless/releases/download/v0.3.0/kubeless-rbac-v0.3.0.yaml | kubectl create -f -
+#check pods
+kubectl get pods -n kubeless
+#The controller which will watch for function objects to be created and also two Pods to enabled PubSub function.
+echo 'def handler(event):
+   print event.json
+   return event.json' > lambda.py
+#specify the runtime, file for the function source code and the trigger
+kubeless function deploy lambda --runtime python2.7 \
+                                --handler lambda.handler \
+                                --from-file lambda.py \
+                                --trigger-http
+#list function
+kubeless function ls
+#Under the hood, Kubeless automatically created a Kubernetes deployment and service. You can check that a Pod containing your function is running
+kubectl get pods 
+kubectl get deployments,services
+#call function
+kubeless function call lambda --data '{"hello":"world"}'
+#run proxy
+kubectl proxy --port 8080 &
+#query 
+curl localhost:8080/custom/api/v1/pods
+
+curl --data '{"hello":"world"}' localhost:8080/api/v1/proxy/namespaces/default/services/lambda:8080/ --header "Content-Type:application/json"
+#get logs
+kubeless function logs lambda
+#update function
+kubeless function update toy --from-file lambda-update.py
+#add js function
+echo 'module.exports = {
+  handler: (req, res) => {
+    console.log(req.body);
+    res.end(JSON.stringify(req.body));
+  },
+};' > hello.js
+kubeless function deploy hello --runtime nodejs6 \
+                               --handler hello.handler \
+                               --from-file hello.js \
+                               --trigger-http
+kubeless function delete lambda
+
