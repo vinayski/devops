0f36d7b57f9b8656312ce21494a094f6c4c9d33d
SKi <vinayski@gmail.com>
2018-01-30 13:36:46 -0500

    Add ansible playbook

---
+++ testans.sh
@@ -0,0 +1,23 @@
+#create host for centos
+echo '[group1]
+host01 ansible_ssh_user=cent'>myhosts
+ansible group1 -i myhosts -m command -a date
+#create playbook to check if latest version of sysstat is installed
+#if older version or not present ansible should install the latest
+echo '---
+- hosts: host01
+
+  become: true
+
+  tasks:
+    - name: ensure latest sysstat is installed
+      apt:
+        name: sysstat
+        state: latest'>site.yml
+ansible-playbook -i myhosts site.yml
+#list docs
+ansible-doc apt
+# remove sysstat pkg
+sed -i -e 's/state: latest/state: absent/' -e 's/ensure.*/ensure sysstat is removed/' site.yml
+ansible-playbook -i myhosts site.yml
+
