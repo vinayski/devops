62174aadd7ad7a25054052aa92bf240cef2e7b9b
SKi <vinayski@gmail.com>
2018-05-30 19:44:27 -0700

    fix vpn issue

--- vpn/vpncheck.sh
+++ vpn/vpncheck.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-export VPN=`ps -C vpnc |wc -l`
-
-if [ $VPN -ge 2 ]; then 
+export pinghost=${1:-10.26.4.1}   
+export VPN=`ps -C vpnc | wc -l`
+export VPNPID=`ps --no-header -C vpnc | cut -c1-6`
+ping -q -c5 $pinghost > /dev/null
+if [ $VPN -ge 2 ] && [ $? -eq 0 ]; then 
   sleep 1
 else 
+  if [ $VPN -ge 2 ]; then 
+	  echo `date` ": kill vpn pid:$VPNPID"
+	  #sudo kill -9 $VPNPID
+  fi
   ~/vpn.sh
-  export VPNPID=`ps --no-header -C vpnc | cut -c1-7`
-  echo `date` ": vpn pid:$VPNPID"
+  sleep 2
+  export VPNPID=`ps --no-header -C vpnc | cut -c1-6`
+  ping -q -c5 $pinghost > /dev/null
+  echo `date` ": vpn pid:$VPNPID $?"
 fi
